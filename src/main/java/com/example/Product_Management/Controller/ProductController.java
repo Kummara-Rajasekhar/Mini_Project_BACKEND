df8a4211557be641e38b4b08429efbec59b7f648
K Rajasekhar <kummararajasekhar17092004@gmail.com>
2025-01-24 17:55:55 +0530

    final day commit

--- src/main/java/com/example/Product_Management/Controller/ProductController.java
+++ src/main/java/com/example/Product_Management/Controller/ProductController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+@CrossOrigin(origins ="http://localhost:8080" )
 @RestController
 public class ProductController {
     private ProductService productService;
